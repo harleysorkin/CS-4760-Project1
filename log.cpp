53ebd0d381d1c3a82ba739e6731dcc07e027e93e
harleysorkin <hsrzg@umsl.edu>
2022-09-14 20:52:02 -0500

    First commit

---
+++ log.cpp
@@ -0,0 +1,96 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <getopt.h>
+#include <iostream>
+#include <fstream>
+
+#include "log.h"
+
+#define  MSGLEN 9999
+
+typedef struct list_struct {
+    data_t item;
+    struct list_struct *next;
+} log_t;
+
+static log_t *headptr = NULL;
+static log_t *tailptr = NULL;
+
+int addmsg(const char type, char *msg) {
+    log_t *nmsg = new log_t;
+
+    if (nmsg == NULL) {
+        perror("Error: addmsg()");
+        return -1;
+    }
+    else {
+        nmsg->item.string = msg;
+        nmsg->item.type = type;
+        nmsg->next = NULL;
+
+        if (headptr == NULL) {
+            headptr = nmsg;
+            tailptr = nmsg;
+        }
+        else {
+            tailptr->next = nmsg;
+            tailptr = nmsg;
+        }
+       
+        return 0;
+    }
+}
+
+void clearlog(void) {
+    
+    log_t *x;
+
+    while (headptr != NULL) {
+        x = headptr;
+        headptr = headptr->next;
+        delete x;
+    }
+    
+    headptr = NULL;
+}
+
+char *getlog(void) {
+
+    char *output = new char[MSGLEN];
+    log_t *x = headptr;
+
+    if (output == NULL) {
+        perror("Error: getlog()");
+        return NULL;
+    }
+    else {
+        strcpy(output, "");
+        while (x != NULL) {
+            strcat(output, (x->item).string);
+            strcat(output, "\n");
+            x = x->next;
+        }
+
+        return output;
+    }
+}
+
+int savelog(char *filename) {
+
+    std::ofstream file;
+    file.open(filename);
+
+    if (!file) {
+        perror("Error: savelog()");
+        return -1;
+    }
+    else {
+        file << getlog();
+        file.close();
+        return 0;
+    }
+
+}
+
+
---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <getopt.h>
+#include <stdio.h>
+#include <iostream>
+
+#include "log.h"
+
+int main (int argc, char **argv) {
+
+    int opt;
+
+    while((opt = getopt(argc, argv, ":ht:")) != -1) {
+        switch(opt) {
+            case 't':
+                std::cout << ("Average time between writes: ") << (optarg) << std::endl;
+                break;
+                        
+            case 'h':
+                std::cout << "Usage: ./driver [-h] [-t sec] [logfile]\nWhere sec is the average amount of time between writes to the logfile (default 2s)\nAnd logfile is the name of the file to be written to (default messages.log)" << std::endl;
+                exit(0);
+
+            case ':':
+                std::cout << ("Option needs a value.\n") << std::endl;
+                exit(0);
+                                
+            default:
+                std::cout << ("Please choose a valid argument.\n") << std::endl;
+                exit(0);
+        }
+    }
+
+}
